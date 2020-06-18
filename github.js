32b88b10ba39341aaacb7176a701f0fa0df12cb5
AlElenaV <56462583+AlElenaV@users.noreply.github.com>
2020-06-18 17:14:21 +1000

    Add files via upload
    output information about users from GitHub with API https://api.github.com/users

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = 'a81377c63908e7648808';
+    this.client_secret = '847c6e366ab444ed4d0b86b907518bcfadf65bfd';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
